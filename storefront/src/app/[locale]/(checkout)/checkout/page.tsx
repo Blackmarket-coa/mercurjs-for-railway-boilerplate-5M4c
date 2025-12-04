49ea41bfba1ac4cb8cb973ecfd93b82daab28f0d
Black Market Crash Dummy <foodcoa@gmail.com>
2025-12-04 16:34:01 -0500

    Update checkout page description

--- storefront/src/app/[locale]/(checkout)/checkout/page.tsx
+++ storefront/src/app/[locale]/(checkout)/checkout/page.tsx
@@ -14,7 +14,7 @@ import { Suspense } from "react"
 
 export const metadata: Metadata = {
   title: "Checkout",
-  description: "My cart page - Checkout",
+  description: "My cart",
 }
 
 export default async function CheckoutPage({}) {
