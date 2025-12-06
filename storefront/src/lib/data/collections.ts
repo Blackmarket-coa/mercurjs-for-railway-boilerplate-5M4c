4855b6140fa5e42c68aa427b2d04da123d124a08
Black Market Crash Dummy <foodcoa@gmail.com>
2025-12-05 23:04:42 -0500

    Update collections.ts

--- storefront/src/lib/data/collections.ts
+++ storefront/src/lib/data/collections.ts
@@ -2,13 +2,14 @@
 import { HttpTypes } from "@medusajs/types"
 import { getCacheOptions } from "./cookies"
 import { sdk } from "../config"
+
 export const retrieveCollection = async (id: string) => {
   const next = {
     ...(await getCacheOptions("collections")),
   }
   return sdk.client
     .fetch<{ collection: HttpTypes.StoreCollection }>(
-      /store/collections/${id},
+      `/store/collections/${id}`,
       {
         next,
         cache: "force-cache",
@@ -16,6 +17,7 @@ export const retrieveCollection = async (id: string) => {
     )
     .then(({ collection }) => collection)
 }
+
 export const listCollections = async (
   queryParams: Record<string, string> = {}
 ): Promise<{ collections: HttpTypes.StoreCollection[]; count: number }> => {
@@ -24,6 +26,7 @@ export const listCollections = async (
   }
   queryParams.limit = queryParams.limit || "100"
   queryParams.offset = queryParams.offset || "0"
+  
   return sdk.client
     .fetch<{ collections: HttpTypes.StoreCollection[]; count: number }>(
       "/store/collections",
@@ -35,16 +38,19 @@ export const listCollections = async (
     )
     .then(({ collections }) => ({ collections, count: collections.length }))
 }
+
 export const getCollectionByHandle = async (
   handle: string
 ): Promise<HttpTypes.StoreCollection> => {
   const next = {
     ...(await getCacheOptions("collections")),
   }
+  
   return sdk.client
-    .fetch<HttpTypes.StoreCollectionListResponse>/store/collections, {
+    .fetch<HttpTypes.StoreCollectionListResponse>(`/store/collections`, {
       query: { handle, fields: "*products" },
       next,
       cache: "force-cache",
     })
     .then(({ collections }) => collections[0])
+}
