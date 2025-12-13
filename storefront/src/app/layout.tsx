77b42969c11b99c2f4bb865a237dd0dcdc27bec4
Black Market Crash Dummy <foodcoa@gmail.com>
2025-12-13 14:38:31 -0800

    Update layout.tsx

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -1,20 +1,15 @@
 // src/app/layout.tsx
 import "./globals.css"
 import { Funnel_Display } from "next/font/google"
+import { CartProvider } from "medusa-react"
 import Head from "next/head"
 
-// Google Font setup
 const funnelDisplay = Funnel_Display({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600"],
   variable: "--font-funnel-sans",
   display: "swap",
 })
-
-export const metadata = {
-  title: "Black Market Coalition Storefront",
-  description: "Secure and reliable B2C marketplace",
-}
 
 export default function RootLayout({
   children,
@@ -28,12 +23,14 @@ export default function RootLayout({
   return (
     <html lang={htmlLang} className={funnelDisplay.variable}>
       <Head>
-        {/* Image & API origins */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </Head>
       <body className="antialiased bg-primary text-secondary relative">
-        {children}
+        {/* Wrap children in CartProvider */}
+        <CartProvider>
+          {children}
+        </CartProvider>
       </body>
     </html>
   )
