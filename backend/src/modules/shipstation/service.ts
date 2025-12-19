372eb0c271ad4140868833b0e300c22b74c90077
Black Market Crash Dummy <foodcoa@gmail.com>
2025-12-19 15:14:05 -0800

    Implement ShipStationProviderService class

---
+++ backend/src/modules/shipstation/service.ts
@@ -0,0 +1,20 @@
+import { AbstractFulfillmentProviderService } from "@medusajs/framework/utils"
+
+export type ShipStationOptions = {
+  api_key: string
+}
+
+class ShipStationProviderService extends AbstractFulfillmentProviderService {
+  static identifier = "shipstation"
+  protected options_: ShipStationOptions
+
+  constructor({}, options: ShipStationOptions) {
+    super()
+
+    this.options_ = options
+  }
+
+  // TODO add methods
+}
+
+export default ShipStationProviderService
