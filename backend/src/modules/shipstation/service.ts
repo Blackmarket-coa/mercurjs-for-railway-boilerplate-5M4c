70a7bef7e2dce45189bbebd4675a91abb069788b
Black Market Crash Dummy <foodcoa@gmail.com>
2025-12-19 15:22:50 -0800

    Add package weight calculation for shipping rates
    Calculate total package weight for shipping rates

--- backend/src/modules/shipstation/service.ts
+++ backend/src/modules/shipstation/service.ts
@@ -138,7 +138,42 @@ class ShipStationProviderService extends AbstractFulfillmentProviderService {
       address_residential_indicator: "unknown",
     }
 
-    // TODO create shipment
+    // Sum the package's weight
+// You can instead create different packages for each item
+const packageWeight = items.reduce((sum, item) => {
+  // @ts-ignore
+  return sum + (item.variant.weight || 0)
+}, 0)
+
+return await this.client.getShippingRates({
+  shipment: {
+    carrier_id: carrier_id,
+    service_code: carrier_service_code,
+    ship_to,
+    ship_from,
+    validate_address: "no_validation",
+    items: items?.map((item) => ({
+      name: item.title,
+      quantity: item.quantity,
+      sku: item.variant_sku || "",
+    })),
+    packages: [{
+      weight: {
+        value: packageWeight,
+        unit: "kilogram",
+      },
+    }],
+    customs: {
+      contents: "merchandise",
+      non_delivery: "return_to_sender",
+    },
+  },
+  rate_options: {
+    carrier_ids: [carrier_id],
+    service_codes: [carrier_service_code],
+    preferred_currency: currency_code as string,
+  },
+})
   }
 }
 
