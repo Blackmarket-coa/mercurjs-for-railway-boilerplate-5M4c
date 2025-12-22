94ab4cee864343207f4adb6be1aa840800c250d9
Black Market Crash Dummy <foodcoa@gmail.com>
2025-12-22 15:32:29 -0800

    Refactor fulfillDigitalOrderWorkflow with new imports

--- backend/src/workflows/fulfill-digital-order/index.ts
+++ backend/src/workflows/fulfill-digital-order/index.ts
@@ -1,8 +1,57 @@
-// src/workflows/fulfill-digital-order/index.ts
-import { createWorkflow } from "@medusajs/workflows-sdk"
-import { fulfillDigitalOrderStep } from "./steps/fulfill-digital-order-step"
+import {
+  createWorkflow,
+  when,
+  WorkflowResponse
+} from "@medusajs/framework/workflows-sdk"
+import {
+  markOrderFulfillmentAsDeliveredWorkflow,
+  useQueryGraphStep,
+} from "@medusajs/medusa/core-flows"
+import { 
+  sendDigitalOrderNotificationStep, 
+  SendDigitalOrderNotificationStepInput
+} from "./steps/send-digital-order-notification"
 
-export const fulfillDigitalOrderWorkflow = createWorkflow({
-  id: "bmc-fulfill-digital-order",
-  steps: [fulfillDigitalOrderStep],
-})
+type FulfillDigitalOrderWorkflowInput = {
+  id: string
+}
+
+export const fulfillDigitalOrderWorkflow = createWorkflow(
+  "fulfill-digital-order",
+  ({ id }: FulfillDigitalOrderWorkflowInput) => {
+    const { data: digitalProductOrders } = useQueryGraphStep({
+      entity: "digital_product_order",
+      fields: [
+        "*",
+        "products.*",
+        "products.medias.*",
+        "order.*",
+        "order.fulfillments.*"
+      ],
+      filters: {
+        id,
+      },
+      options: {
+        throwIfKeyNotFound: true
+      }
+    })
+
+    sendDigitalOrderNotificationStep({
+      digital_product_order: digitalProductOrders[0]
+    } as unknown as SendDigitalOrderNotificationStepInput)
+
+    when({ digitalProductOrders }, (data) => !!data.digitalProductOrders[0].order?.fulfillments?.length)
+      .then(() => {
+        markOrderFulfillmentAsDeliveredWorkflow.runAsStep({
+          input: {
+            orderId: digitalProductOrders[0].order!.id,
+            fulfillmentId: digitalProductOrders[0].order!.fulfillments![0]!.id
+          }
+        })
+      })
+
+    return new WorkflowResponse(
+      digitalProductOrders[0]
+    )
+  }
+)
