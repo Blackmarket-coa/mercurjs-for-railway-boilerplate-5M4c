43a3b77666367b9dfde7acda20ddaf7d7d8de4f7
Black Market Crash Dummy <foodcoa@gmail.com>
2025-12-22 14:29:08 -0800

    Add files via upload

--- backend/src/workflows/fulfill-digital-order/steps/send-digital-order-notification.ts
+++ backend/src/workflows/fulfill-digital-order/steps/send-digital-order-notification.ts
@@ -1,29 +1,74 @@
-import { createStep, StepResponse } from "@medusajs/framework/workflows-sdk"
-import { MedusaError } from "@medusajs/framework/utils"
-import { TICKET_BOOKING_MODULE } from "../../modules/ticket-booking"
-import type TicketBookingModuleService from "../../modules/ticket-booking/service"
+import {
+  createStep,
+  StepResponse
+} from "@medusajs/framework/workflows-sdk"
+import { 
+  INotificationModuleService,
+  IFileModuleService
+} from "@medusajs/framework/types"
+import { MedusaError, ModuleRegistrationName, promiseAll } from "@medusajs/framework/utils"
+import { DigitalProductOrder, MediaType } from "../../../modules/digital-product/types"
 
-export type VerifyTicketPurchaseStepInput = {
-  ticket_purchase_id: string
+export type SendDigitalOrderNotificationStepInput = {
+  digital_product_order: DigitalProductOrder
 }
 
-export const verifyTicketPurchaseStep = createStep(
-  "verify-ticket-purchase",
-  async (input: VerifyTicketPurchaseStepInput, { container }) => {
-    const ticketBookingService = container.resolve(
-      TICKET_BOOKING_MODULE
-    ) as TicketBookingModuleService
+export const sendDigitalOrderNotificationStep = createStep(
+  "send-digital-order-notification",
+  async ({ 
+    digital_product_order: digitalProductOrder 
+  }: SendDigitalOrderNotificationStepInput, 
+  { container }) => {
+    const notificationModuleService: INotificationModuleService = container
+    .resolve(ModuleRegistrationName.NOTIFICATION)
+    const fileModuleService: IFileModuleService = container.resolve(
+      ModuleRegistrationName.FILE
+    )
 
-    const ticketPurchase = await ticketBookingService.retrieveTicketPurchase(input.ticket_purchase_id)
-
-    if (ticketPurchase.status !== "pending") {
-      throw new MedusaError(MedusaError.Types.NOT_ALLOWED, "Ticket has already been scanned")
+    if (!digitalProductOrder.order) {
+      throw new MedusaError(
+        MedusaError.Types.INVALID_DATA,
+        "Digital product order is missing associated order."
+      )
     }
 
-    if (ticketPurchase.show_date < new Date()) {
-      throw new MedusaError(MedusaError.Types.NOT_ALLOWED, "Ticket is expired or show date has passed")
+    if (!digitalProductOrder.order.email) {
+      throw new MedusaError(
+        MedusaError.Types.INVALID_DATA,
+        "Order is missing email."
+      )
     }
 
-    return new StepResponse(true)
+    const notificationData = await promiseAll(
+      digitalProductOrder.products.map(async (product) => {
+        const medias: string[] = []
+  
+        await promiseAll(
+          product.medias
+          .filter((media) => media.type === MediaType.MAIN)
+          .map(async (media) => {
+            medias.push(
+              (await fileModuleService.retrieveFile(media.fileId)).url
+            )
+          })
+        )
+  
+        return {
+          name: product.name,
+          medias
+        }
+      })
+    )
+  
+    const notification = await notificationModuleService.createNotifications({
+      to: digitalProductOrder.order.email,
+      template: "digital-order-template",
+      channel: "email",
+      data: {
+        products: notificationData
+      }
+    })
+
+    return new StepResponse(notification)
   }
 )
