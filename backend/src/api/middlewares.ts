eca9fd1290f8ff7616ee8870a931f77a44d92e73
Black Market Crash Dummy <foodcoa@gmail.com>
2025-12-22 12:23:09 -0800

    Implement query validation for admin venues
    Added validateAndTransformQuery middleware for querying venues.

--- backend/src/api/middlewares.ts
+++ backend/src/api/middlewares.ts
@@ -1,4 +1,5 @@
-import { 
+import { validateAndTransformQuery } from "@medusajs/framework/http"
+import { createFindParams } from "@medusajs/medusa/api/utils/validators"import { 
   defineMiddlewares, 
   validateAndTransformBody,
 } from "@medusajs/framework/http"
@@ -11,6 +12,21 @@ export default defineMiddlewares({
       methods: ["POST"],
       middlewares: [
         validateAndTransformBody(CreateVenueSchema),
+      ],
+    },
+  ],
+})
+export default defineMiddlewares({
+  routes: [
+    // ...
+    {
+      matcher: "/admin/venues",
+      methods: ["GET"],
+      middlewares: [
+        validateAndTransformQuery(createFindParams(), {
+          isList: true,
+          defaults: ["id", "name", "address", "rows.*"],
+        }),
       ],
     },
   ],
