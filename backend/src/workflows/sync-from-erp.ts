18fda67d799c5804c0be0876d1a7839cae097b88
Black Market Crash Dummy <foodcoa@gmail.com>
2025-12-24 14:13:24 -0500

    Update sync-from-erp.ts

--- backend/src/workflows/sync-from-erp.ts
+++ backend/src/workflows/sync-from-erp.ts
@@ -1,4 +1,18 @@
 import { createStep, StepResponse } from "@medusajs/framework/workflows-sdk"
+import {
+  createWorkflow,
+  transform,
+  WorkflowResponse,
+} from "@medusajs/framework/workflows-sdk"
+import {
+  createProductsWorkflow,
+  updateProductsWorkflow,
+  useQueryGraphStep,
+} from "@medusajs/medusa/core-flows"
+import {
+  CreateProductWorkflowInputDTO,
+  UpdateProductWorkflowInputDTO,
+} from "@medusajs/framework/types"
 
 type Input = {
   offset: number
@@ -15,15 +29,7 @@ const getProductsFromErp = createStep(
     return new StepResponse(products)
   }
 )
-  import { 
-  createWorkflow, transform, WorkflowResponse,
-} from "@medusajs/framework/workflows-sdk"
-import { 
-  createProductsWorkflow, updateProductsWorkflow, useQueryGraphStep,
-} from "@medusajs/medusa/core-flows"
-import { 
-  CreateProductWorkflowInputDTO, UpdateProductWorkflowInputDTO,
-} from "@medusajs/framework/types"
+
 export const syncFromErpWorkflow = createWorkflow(
   "sync-from-erp",
   (input: Input) => {
@@ -32,9 +38,7 @@ export const syncFromErpWorkflow = createWorkflow(
     // @ts-ignore
     const { data: stores } = useQueryGraphStep({
       entity: "store",
-      fields: [
-        "default_sales_channel_id",
-      ],
+      fields: ["default_sales_channel_id"],
     })
 
     // @ts-ignore
@@ -46,11 +50,14 @@ export const syncFromErpWorkflow = createWorkflow(
       },
     }).config({ name: "shipping-profile" })
 
-    const externalIdsFilters = transform({
-      odooProducts,
-    }, (data) => {
-      return data.odooProducts.map((product) => `${product.id}`)
-    })
+    const externalIdsFilters = transform(
+      {
+        odooProducts,
+      },
+      (data) => {
+        return data.odooProducts.map((product) => `${product.id}`)
+      }
+    )
 
     // @ts-ignore
     const { data: existingProducts } = useQueryGraphStep({
@@ -62,128 +69,140 @@ export const syncFromErpWorkflow = createWorkflow(
       },
     }).config({ name: "existing-products" })
 
-    const {
-  productsToCreate,
-  productsToUpdate,
-} = transform({
-  existingProducts,
-  odooProducts,
-  shippingProfiles,
-  stores,
-}, (data) => {
-  const productsToCreate: CreateProductWorkflowInputDTO[] = []
-  const productsToUpdate: UpdateProductWorkflowInputDTO[] = []
+    const { productsToCreate, productsToUpdate } = transform(
+      {
+        existingProducts,
+        odooProducts,
+        shippingProfiles,
+        stores,
+      },
+      (data) => {
+        const productsToCreate: CreateProductWorkflowInputDTO[] = []
+        const productsToUpdate: UpdateProductWorkflowInputDTO[] = []
 
-  data.odooProducts.forEach((odooProduct) => {
-    const product: CreateProductWorkflowInputDTO | UpdateProductWorkflowInputDTO = {
-      external_id: `${odooProduct.id}`,
-      title: odooProduct.display_name,
-      description: odooProduct.description || odooProduct.description_sale || "",
-      status: odooProduct.is_published ? "published" : "draft",
-      options: odooProduct.attribute_line_ids.length ? odooProduct.attribute_line_ids.map((attribute) => {
+        data.odooProducts.forEach((odooProduct) => {
+          const product:
+            | CreateProductWorkflowInputDTO
+            | UpdateProductWorkflowInputDTO = {
+            external_id: `${odooProduct.id}`,
+            title: odooProduct.display_name,
+            description:
+              odooProduct.description || odooProduct.description_sale || "",
+            status: odooProduct.is_published ? "published" : "draft",
+            options: odooProduct.attribute_line_ids.length
+              ? odooProduct.attribute_line_ids.map((attribute) => {
+                  return {
+                    title: attribute.attribute_id.display_name,
+                    values: attribute.value_ids.map(
+                      (value) => value.display_name
+                    ),
+                  }
+                })
+              : [
+                  {
+                    title: "Default",
+                    values: ["Default"],
+                  },
+                ],
+            hs_code: odooProduct.hs_code || "",
+            handle: odooProduct.website_url.replace("/shop/", ""),
+            variants: [],
+            shipping_profile_id: data.shippingProfiles[0].id,
+            sales_channels: [
+              {
+                id: data.stores[0].default_sales_channel_id || "",
+              },
+            ],
+          }
+
+          const existingProduct = data.existingProducts.find(
+            (p) => p.external_id === product.external_id
+          )
+          if (existingProduct) {
+            product.id = existingProduct.id
+          }
+
+          if (odooProduct.product_variant_ids?.length) {
+            product.variants = odooProduct.product_variant_ids.map((variant) => {
+              const options: Record<string, string> = {}
+              if (variant.product_template_variant_value_ids.length) {
+                variant.product_template_variant_value_ids.forEach((value) => {
+                  options[value.attribute_id.display_name] = value.name
+                })
+              } else {
+                product.options?.forEach((option) => {
+                  options[option.title] = option.values[0]
+                })
+              }
+              return {
+                id: existingProduct
+                  ? existingProduct.variants.find((v) => v.sku === variant.code)
+                      ?.id
+                  : undefined,
+                title: variant.display_name.replace(`[${variant.code}] `, ""),
+                sku: variant.code || undefined,
+                options,
+                prices: [
+                  {
+                    amount: variant.list_price,
+                    currency_code: variant.currency_id.display_name.toLowerCase(),
+                  },
+                ],
+                manage_inventory: false, // change to true if syncing inventory from Odoo
+                metadata: {
+                  external_id: `${variant.id}`,
+                },
+              }
+            })
+          } else {
+            product.variants?.push({
+              id: existingProduct ? existingProduct.variants[0].id : undefined,
+              title: "Default",
+              options: {
+                Default: "Default",
+              },
+              // @ts-ignore
+              prices: [
+                {
+                  amount: odooProduct.list_price,
+                  currency_code: odooProduct.currency_id.display_name.toLowerCase(),
+                },
+              ],
+              metadata: {
+                external_id: `${odooProduct.id}`,
+              },
+              manage_inventory: false, // change to true if syncing inventory from Odoo
+            })
+          }
+
+          if (existingProduct) {
+            productsToUpdate.push(product as UpdateProductWorkflowInputDTO)
+          } else {
+            productsToCreate.push(product as CreateProductWorkflowInputDTO)
+          }
+        })
+
         return {
-          title: attribute.attribute_id.display_name,
-          values: attribute.value_ids.map((value) => value.display_name),
+          productsToCreate,
+          productsToUpdate,
         }
-      }) : [
-        {
-          title: "Default",
-          values: ["Default"],
-        },
-      ],
-      hs_code: odooProduct.hs_code || "",
-      handle: odooProduct.website_url.replace("/shop/", ""),
-      variants: [],
-      shipping_profile_id: data.shippingProfiles[0].id,
-      sales_channels: [
-        {
-          id: data.stores[0].default_sales_channel_id || "",
-        },
-      ],
-    }
+      }
+    )
 
-    const existingProduct = data.existingProducts.find((p) => p.external_id === product.external_id)
-    if (existingProduct) {
-      product.id = existingProduct.id
-    }
+    createProductsWorkflow.runAsStep({
+      input: {
+        products: productsToCreate,
+      },
+    })
 
-    if (odooProduct.product_variant_ids?.length) {
-      product.variants = odooProduct.product_variant_ids.map((variant) => {
-        const options = {}
-        if (variant.product_template_variant_value_ids.length) {
-          variant.product_template_variant_value_ids.forEach((value) => {
-            options[value.attribute_id.display_name] = value.name
-          })
-        } else {
-          product.options?.forEach((option) => {
-            options[option.title] = option.values[0]
-          })
-        }
-        return {
-          id: existingProduct ? existingProduct.variants.find((v) => v.sku === variant.code)?.id : undefined,
-          title: variant.display_name.replace(`[${variant.code}] `, ""),
-          sku: variant.code || undefined,
-          options,
-          prices: [
-            {
-              amount: variant.list_price,
-              currency_code: variant.currency_id.display_name.toLowerCase(),
-            },
-          ],
-          manage_inventory: false, // change to true if syncing inventory from Odoo
-          metadata: {
-            external_id: `${variant.id}`,
-          },
-        }
-      })
-    } else {
-      product.variants?.push({
-        id: existingProduct ? existingProduct.variants[0].id : undefined,
-        title: "Default",
-        options: {
-          Default: "Default",
-        },
-        // @ts-ignore
-        prices: [
-          {
-            amount: odooProduct.list_price,
-            currency_code: odooProduct.currency_id.display_name.toLowerCase(),
-          },
-        ],
-        metadata: {
-          external_id: `${odooProduct.id}`,
-        },
-        manage_inventory: false, // change to true if syncing inventory from Odoo
-      })
-    }
+    updateProductsWorkflow.runAsStep({
+      input: {
+        products: productsToUpdate,
+      },
+    })
 
-    if (existingProduct) {
-      productsToUpdate.push(product as UpdateProductWorkflowInputDTO)
-    } else {
-      productsToCreate.push(product as CreateProductWorkflowInputDTO)
-    }
-  })
-
-  return {
-    productsToCreate,
-    productsToUpdate,
-  }
-})
-
-createProductsWorkflow.runAsStep({
-  input: {
-    products: productsToCreate,
-  },
-})
-
-updateProductsWorkflow.runAsStep({
-  input: {
-    products: productsToUpdate,
-  },
-})
-
-return new WorkflowResponse({
-  odooProducts,
-})
+    return new WorkflowResponse({
+      odooProducts,
+    })
   }
 )
