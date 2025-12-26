90ed1db86e0bdc8d8ccc813fce3e95d6089ed4d1
Black Market Crash Dummy <foodcoa@gmail.com>
2025-12-25 21:11:20 -0500

    Add VenuesPage component with heading and description

---
+++ vendor-panel/src/routes/venues/page.tsx
@@ -0,0 +1,10 @@
+import { Container, Heading } from "@medusajs/ui"
+
+export default function VenuesPage() {
+  return (
+    <Container>
+      <Heading level="h1">Venues</Heading>
+      <p>Venues page is now registered.</p>
+    </Container>
+  )
+}
