fad47b674f96739d9d3750c0b14245f574249983
Black Market Crash Dummy <foodcoa@gmail.com>
2025-12-27 02:53:38 -0500

    hooks

--- vendor-panel/src/hooks/api/order-cycles.ts
+++ vendor-panel/src/hooks/api/order-cycles.ts
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
-import { sdk } from "../../lib/sdk"
+import { sdk } from "../../lib/client"
 
 // Types
 export interface OrderCycle {
