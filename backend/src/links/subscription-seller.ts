613a7020e52aaed4f2166395e8ff58f78e59eca5
Black Market Crash Dummy <foodcoa@gmail.com>
2026-01-02 18:08:43 -0500

    link audit fix

--- backend/src/links/subscription-seller.ts
+++ backend/src/links/subscription-seller.ts
@@ -1,15 +1,31 @@
 import { defineLink } from "@medusajs/framework/utils"
 import SubscriptionModule from "../modules/subscription"
-import SellerModule from "@mercurjs/b2c-core/modules/seller"
 
 /**
  * Link subscriptions to sellers
  * A seller can have multiple active subscriptions
  */
-export default defineLink(
-  {
-    linkable: SubscriptionModule.linkable.subscription.id,
-    isList: true
-  },
-  SellerModule.linkable.seller
-)
+
+let SellerModule: any
+try {
+  SellerModule = require("@mercurjs/framework").SellerModule
+} catch {
+  try {
+    SellerModule = require("@mercurjs/b2c-core/modules/seller").default
+  } catch {
+    console.warn("No seller module found - subscription-seller links will not be created")
+    SellerModule = null
+  }
+}
+
+const subscriptionSellerLink = SellerModule
+  ? defineLink(
+      {
+        linkable: SubscriptionModule.linkable.subscription.id,
+        isList: true
+      },
+      SellerModule.linkable.seller
+    )
+  : null
+
+export default subscriptionSellerLink
