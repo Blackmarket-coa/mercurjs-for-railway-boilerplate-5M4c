7939fe7180bb59b96ce6d159f6ba11637da88963
Black Market Crash Dummy <foodcoa@gmail.com>
2026-01-27 21:39:30 -0500

    Harden vendor sellers/me field parsing

--- vendor-panel/src/hooks/api/users.tsx
+++ vendor-panel/src/hooks/api/users.tsx
@@ -113,7 +113,7 @@ export const useMe = (
         method: "GET",
         query: {
           fields:
-            "id,name,description,phone,email,media,address_line,postal_code,country_code,city,region,metadata,tax_id,photo,store_status,website_url,social_links,storefront_links",
+            "id,name,description,phone,email,media,address_line,postal_code,country_code,city,metadata,tax_id,photo,store_status,website_url,social_links,storefront_links",
         },
       })
     },
